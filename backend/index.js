5b6a11e422e96ac63ab3ec769b9c9d060fb6ff53
RdhYuGaan <radithyugan22@gmail.com>
2025-04-21 11:52:57 +0530

    connect backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/auth', authRouter);
+
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running ');
 
 }
 );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 
 const Register = () => {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const responce = await axios.post("http://localhost:3000/auth/register", values)
+            console.log(responce)
+        } catch (error) {
+            console.log(error)
+        }
+
+
+    }
     return (
         <div className='flex justify-center items-center h-screen'>
             <div className='shadow-lg  px-8 py-5 border w-96'>
                 <h2 className='text-lg  font-bold mb-4'>Register</h2>
-                <form>
+                <form
+                    onSubmit={handleSubmit}>
                     <div className='mb-4'>
                         <label
                             htmlFor="name"
@@ -16,6 +44,8 @@ const Register = () => {
                             type="text"
                             className='w-full px-3 py-2 border rounded'
                             placeholder="Enter your name"
+                            name="name"
+                            onChange={handleChanges}
                         />
                     </div>
                     <div className='mb-4'>
@@ -27,6 +57,8 @@ const Register = () => {
                             type="email"
                             className='w-full px-3 py-2 border rounded'
                             placeholder="Enter your email"
+                            name="email"
+                            onChange={handleChanges}
                         />
 
                     </div>
@@ -38,7 +70,11 @@ const Register = () => {
                         <input
                             type="password"
                             placeholder="Enter your password"
-                            className='w-full px-3 py-2 border rounded' />
+                            className='w-full px-3 py-2 border rounded'
+                            name="password"
+                            onChange={handleChanges}
+
+                        />
                     </div>
                     <button
                         type="submit"
@@ -48,7 +84,7 @@ const Register = () => {
                 </form>
                 <div className='text-center'>
                     <span>Already have an account? </span>
-                    <a href="/login">Login</a>
+                    <Link to="/login" className='text-blue-500 hover:underline'>Login</Link>
                 </div>
 
             </div>
